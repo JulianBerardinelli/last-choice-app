8d017995637e640c5dd780f1f143a8764b0fb944
Julián Berardinelli <jberardinelli@MacBook-Pro-de-Julian.local>
2024-09-24 12:01:39 +0200

    Fixing bug

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
 // app/page.js
+/* eslint-disable react/no-unescaped-entities */
+
 import Image from 'next/image';
 import Title from './components/Title.js';
 
@@ -18,9 +20,10 @@ export default function HomePage() {
         <div className="flex flex-col justify-center items-center gap-6 text-appear absolute inset-0 z-0 transform translate-y-[-10%]">
           <Title />
           <p className="text-xl font-light text-blue-200 text-center">
-            The home <span className="font-semibold text-red-500">always wins</span>, Don't forget it. <br />
+            The home <span className="font-semibold text-red-500">always wins</span>, Don&apos;t forget it. <br />
             But now, you are at <span className="font-bold text-green-500">home</span>
           </p>
+
         </div>
       </main>
     </>
